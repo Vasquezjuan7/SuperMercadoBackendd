90fe53faaf4aafd39719dfe174a02788c8cb61e1
Vasquezz <juanvasquezsanchez2006@gmail.com>
2026-03-27 14:22:09 -0500

    Update ProductRepository.java

--- cloud-service/src/main/java/com/supermercado/cloud_service/repository/ProductRepository.java
+++ cloud-service/src/main/java/com/supermercado/cloud_service/repository/ProductRepository.java
@@ -1,8 +1,8 @@
 package com.supermercado.cloud_service.repository;
 
 import org.springframework.data.mongodb.repository.MongoRepository;
-import com.supermercado.cloud_service.model.Product; // Importa el modelo ya en inglés
+import com.supermercado.cloud_service.model.Product; 
 
-// La interfaz debe llamarse igual que el archivo físico
+
 public interface ProductRepository extends MongoRepository<Product, String> {
 }
