9d2d62208d303e661746b4b69fad71aaa912bd0a
Vasquezz <juanvasquezsanchez2006@gmail.com>
2026-03-27 14:33:00 -0500

    Update ProductController.java

--- cloud-service/src/main/java/com/supermercado/cloud_service/controller/ProductController.java
+++ cloud-service/src/main/java/com/supermercado/cloud_service/controller/ProductController.java
@@ -7,8 +7,8 @@ import com.supermercado.cloud_service.model.Product;
 import com.supermercado.cloud_service.repository.ProductRepository;
 
 @RestController
-@RequestMapping("/api/products") // Cambiado a inglés
-// CORS configurado para permitir la comunicación con Vercel
+@RequestMapping("/api/products") 
+
 @CrossOrigin(origins = "*", methods = {RequestMethod.GET, RequestMethod.POST, RequestMethod.DELETE, RequestMethod.PUT})
 public class ProductController {
 
@@ -23,10 +23,10 @@ public class ProductController {
     @PostMapping
     public Product saveProduct(@RequestBody Product newProduct) {
         try {
-            // Recibe 'name' y 'aisle' desde el Frontend en React
+
             return productRepository.save(newProduct);
         } catch (Exception e) {
-            // Imprime el error en la consola de Railway/VS Code si algo falla con MongoDB Atlas
+
             System.out.println("ERROR SAVING TO ATLAS: " + e.getMessage());
             throw e;
         }
